3944840aef728ffad2e8d07931e31952233d726e
splichal07 <159157499+splichal07@users.noreply.github.com>
2025-06-18 11:48:17 -0300

    17/06/2025

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# gerador
+# geradordesenha
+gerador de senha
---
+++ main.js
@@ -0,0 +1,37 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12
+ 
+const botoes = document.querySelectorAll('.parametro-senha__botao')
+botoes[0].onclick = diminuirTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuirTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+};
+
+function aumentaTamanho(){
+    if(tamanhoSenha <20){
+        tamanhoSenha ++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+};
+
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+geraSenha();
+
+function geraSenha(){
+    let senha = '';
+    for(let i = 0; i < tamanhoSenha; i ++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    } 
+    campoSenha.value = senha;
+}
